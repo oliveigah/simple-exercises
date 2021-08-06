0e2f3eb3095ea5ac387823877b01c3044a678ce9
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2021-08-06 07:52:06 -0300

    improve AOC 2020 1 from N^2 to N log(N)

--- lib/advent-of-code/1/execute.ex
+++ lib/advent-of-code/1/execute.ex
@@ -2,15 +2,24 @@ defmodule AoC.Challenge1 do
   def run do
     get_input()
     |> Enum.map(&String.to_integer/1)
-    |> check_valid_responses
+    |> check_valid_responses([])
     |> Enum.filter(fn {_, _, is_valid} -> is_valid end)
     |> Enum.map(fn {n1, n2, _} -> n1 * n2 end)
+    |> List.first()
+    |> IO.inspect()
   end
 
-  defp check_valid_responses(input) do
-    for i <- input, j <- input do
-      {i, j, i + j == 2020}
-    end
+  defp check_valid_responses([_h | []], acc_list) do
+    acc_list
+  end
+
+  defp check_valid_responses([current_input | remaining_inputs], acc_list) do
+    current_results =
+      Enum.map(remaining_inputs, fn remaining_input ->
+        {current_input, remaining_input, current_input + remaining_input == 2020}
+      end)
+
+    check_valid_responses(remaining_inputs, current_results ++ acc_list)
   end
 
   defp get_input do
